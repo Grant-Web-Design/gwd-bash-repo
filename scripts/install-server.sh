1f5dbba9104179be91af8aee597b54cb85eff384
joepgruter <joepgruter@gmail.com>
2022-10-14 20:06:40 +0200

    Sudo check

--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -2,3 +2,10 @@
 
 echo "This is the Grant Web Design install server script"
 ls
+
+timeout 2 sudo id && sudo="true" || sudo="false"
+echo "$sudo"
+
+echo "Checking if we're running with sudo rights"
+
+timeout -s SIGKILL 5s sudo -v && (echo "Sudo rights enabled" ; exit 0) || (echo "No sudo rights, please run this script with sudo" ; exit 1)
